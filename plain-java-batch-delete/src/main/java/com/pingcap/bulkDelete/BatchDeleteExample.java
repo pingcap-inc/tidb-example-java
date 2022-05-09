d48066acbab9af5d42a6938f7db2ccc8a8579335
Icemap <qizhi.wang@pingcap.com>
2022-05-09 14:40:22 +0800

    [Update] add batch delete and update examples

---
+++ plain-java-batch-delete/src/main/java/com/pingcap/bulkDelete/BatchDeleteExample.java
@@ -0,0 +1,66 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.pingcap.bulkDelete;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+public class BatchDeleteExample
+{
+    public static void main(String[] args) throws InterruptedException {
+        // Configure the example database connection.
+
+        // Create a mysql data source instance.
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+
+        // Set server name, port, database name, username and password.
+        mysqlDataSource.setServerName("localhost");
+        mysqlDataSource.setPortNumber(4000);
+        mysqlDataSource.setDatabaseName("bookshop");
+        mysqlDataSource.setUser("root");
+        mysqlDataSource.setPassword("");
+
+        while (true) {
+            batchDelete(mysqlDataSource);
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+
+    public static void batchDelete (MysqlDataSource ds) {
+        try (Connection connection = ds.getConnection()) {
+            String sql = "DELETE FROM `bookshop`.`ratings` WHERE `rated_at` >= ? AND  `rated_at` <= ? LIMIT 1000";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.MILLISECOND, 0);
+
+            calendar.set(2022, Calendar.APRIL, 15, 0, 0, 0);
+            preparedStatement.setTimestamp(1, new Timestamp(calendar.getTimeInMillis()));
+
+            calendar.set(2022, Calendar.APRIL, 15, 0, 15, 0);
+            preparedStatement.setTimestamp(2, new Timestamp(calendar.getTimeInMillis()));
+
+            int count = preparedStatement.executeUpdate();
+            System.out.println("delete " + count + " data");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
