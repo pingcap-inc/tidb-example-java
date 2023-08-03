6bea2a39e8ef15ffdcd0fcb45979885c5f2436ab
Icemap <qizhi.wang@pingcap.com>
2023-08-03 13:37:01 +0800

    feat: plain-java-hibernate example update

--- plain-java-hibernate/src/main/java/com/pingcap/HibernateExample.java
+++ plain-java-hibernate/src/main/java/com/pingcap/HibernateExample.java
@@ -14,10 +14,6 @@
 
 package com.pingcap;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import org.hibernate.JDBCException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -29,63 +25,90 @@ import org.hibernate.query.Query;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 import java.util.function.Function;
 
-@Entity
-@Table(name = "player_hibernate")
-class PlayerBean {
-    @Id
-    private String id;
-    @Column(name = "coins")
-    private Integer coins;
-    @Column(name = "goods")
-    private Integer goods;
-
-    public PlayerBean() {
-    }
-
-    public PlayerBean(String id, Integer coins, Integer goods) {
-        this.id = id;
-        this.coins = coins;
-        this.goods = goods;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Integer getCoins() {
-        return coins;
-    }
-
-    public void setCoins(Integer coins) {
-        this.coins = coins;
-    }
-
-    public Integer getGoods() {
-        return goods;
-    }
-
-    public void setGoods(Integer goods) {
-        this.goods = goods;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("    %-8s => %10s\n    %-8s => %10s\n    %-8s => %10s\n",
-                "id", this.id, "coins", this.coins, "goods", this.goods);
-    }
-}
 
 /**
  * Main class for the basic Hibernate example.
  **/
 public class HibernateExample
 {
+    public static void main(String[] args) {
+        // 1. Create a SessionFactory based on our hibernate.cfg.xml configuration
+        // file, which defines how to connect to the database.
+        SessionFactory sessionFactory
+                = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .setProperties(getTiDBProperties())
+                .addAnnotatedClass(PlayerBean.class)
+                .buildSessionFactory();
+
+        try (Session session = sessionFactory.openSession()) {
+            // 2. And then, create DAO to manager your data.
+            PlayerDAO playerDAO = new PlayerDAO();
+
+            // 3. Run some simple examples.
+
+            // Create a player who has 1 coin and 1 goods.
+            playerDAO.runTransaction(session, playerDAO.createPlayers(Collections.singletonList(
+                    new PlayerBean("test", 1, 1))));
+
+            // Get a player.
+            PlayerBean testPlayer = (PlayerBean)playerDAO.runTransaction(session, playerDAO.getPlayerByID("test"));
+            System.out.printf("PlayerDAO.getPlayer:\n    => id: %s\n    => coins: %s\n    => goods: %s\n",
+                    testPlayer.getId(), testPlayer.getCoins(), testPlayer.getGoods());
+
+            // Count players amount.
+            Long count = (Long)playerDAO.runTransaction(session, playerDAO.countPlayers());
+            System.out.printf("PlayerDAO.countPlayers:\n    => %d total players\n", count);
+
+            // Print 3 players.
+            playerDAO.runTransaction(session, playerDAO.printPlayers(3));
+
+            // 4. Getting further.
+
+            // Player 1: id is "1", has only 100 coins.
+            // Player 2: id is "2", has 114514 coins, and 20 goods.
+            PlayerBean player1 = new PlayerBean("1", 100, 0);
+            PlayerBean player2 = new PlayerBean("2", 114514, 20);
+
+            // Create two players "by hand", using the INSERT statement on the backend.
+            int addedCount = (Integer)playerDAO.runTransaction(session,
+                    playerDAO.createPlayers(Arrays.asList(player1, player2)));
+            System.out.printf("PlayerDAO.createPlayers:\n    => %d total inserted players\n", addedCount);
+
+            // Player 1 wants to buy 10 goods from player 2.
+            // It will cost 500 coins, but player 1 cannot afford it.
+            System.out.println("\nPlayerDAO.buyGoods:\n    => this trade will fail");
+            Integer updatedCount = (Integer)playerDAO.runTransaction(session,
+                    playerDAO.buyGoods(player2.getId(), player1.getId(), 10, 500));
+            System.out.printf("PlayerDAO.buyGoods:\n    => %d total update players\n", updatedCount);
+
+            // So player 1 has to reduce the incoming quantity to two.
+            System.out.println("\nPlayerDAO.buyGoods:\n    => this trade will success");
+            updatedCount = (Integer)playerDAO.runTransaction(session,
+                    playerDAO.buyGoods(player2.getId(), player1.getId(), 2, 100));
+            System.out.printf("PlayerDAO.buyGoods:\n    => %d total update players\n", updatedCount);
+        } finally {
+            sessionFactory.close();
+        }
+    }
+
+    public static Properties getTiDBProperties() {
+        // 1. Get TiDB connection properties from environment variables.
+        String tidbJDBCURL = System.getenv().getOrDefault("TIDB_JDBC_URL", "jdbc:mysql://localhost:4000/test");
+        String tidbUser = System.getenv().getOrDefault("TIDB_USER", "root");
+        String tidbPassword = System.getenv().getOrDefault("TIDB_PASSWORD", "");
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.connection.url", tidbJDBCURL);
+        properties.setProperty("hibernate.connection.username", tidbUser);
+        properties.setProperty("hibernate.connection.password", tidbPassword);
+
+        return properties;
+    }
+
     public static class PlayerDAO {
         public static class NotEnoughException extends RuntimeException {
             public NotEnoughException(String message) {
@@ -94,7 +117,7 @@ public class HibernateExample
         }
 
         // Run SQL code in a way that automatically handles the
-        // transaction retry logic so we don't have to duplicate it in
+        // transaction retry logic, so we don't have to duplicate it in
         // various places.
         public Object runTransaction(Session session, Function<Session, Object> fn) {
             Object resultObject = null;
@@ -177,66 +200,6 @@ public class HibernateExample
                 Query<Long> countQuery = session.createQuery("SELECT count(player_hibernate) FROM PlayerBean player_hibernate", Long.class);
                 return countQuery.getSingleResult();
             };
-        }
-    }
-
-    public static void main(String[] args) {
-        // 1. Create a SessionFactory based on our hibernate.cfg.xml configuration
-        // file, which defines how to connect to the database.
-        SessionFactory sessionFactory
-                = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(PlayerBean.class)
-                .buildSessionFactory();
-
-        try (Session session = sessionFactory.openSession()) {
-            // 2. And then, create DAO to manager your data.
-            PlayerDAO playerDAO = new PlayerDAO();
-
-            // 3. Run some simple examples.
-
-            // Create a player who has 1 coin and 1 goods.
-            playerDAO.runTransaction(session, playerDAO.createPlayers(Collections.singletonList(
-                    new PlayerBean("test", 1, 1))));
-
-            // Get a player.
-            PlayerBean testPlayer = (PlayerBean)playerDAO.runTransaction(session, playerDAO.getPlayerByID("test"));
-            System.out.printf("PlayerDAO.getPlayer:\n    => id: %s\n    => coins: %s\n    => goods: %s\n",
-                    testPlayer.getId(), testPlayer.getCoins(), testPlayer.getGoods());
-
-            // Count players amount.
-            Long count = (Long)playerDAO.runTransaction(session, playerDAO.countPlayers());
-            System.out.printf("PlayerDAO.countPlayers:\n    => %d total players\n", count);
-
-            // Print 3 players.
-            playerDAO.runTransaction(session, playerDAO.printPlayers(3));
-
-            // 4. Getting further.
-
-            // Player 1: id is "1", has only 100 coins.
-            // Player 2: id is "2", has 114514 coins, and 20 goods.
-            PlayerBean player1 = new PlayerBean("1", 100, 0);
-            PlayerBean player2 = new PlayerBean("2", 114514, 20);
-
-            // Create two players "by hand", using the INSERT statement on the backend.
-            int addedCount = (Integer)playerDAO.runTransaction(session,
-                    playerDAO.createPlayers(Arrays.asList(player1, player2)));
-            System.out.printf("PlayerDAO.createPlayers:\n    => %d total inserted players\n", addedCount);
-
-            // Player 1 wants to buy 10 goods from player 2.
-            // It will cost 500 coins, but player 1 cannot afford it.
-            System.out.println("\nPlayerDAO.buyGoods:\n    => this trade will fail");
-            Integer updatedCount = (Integer)playerDAO.runTransaction(session,
-                    playerDAO.buyGoods(player2.getId(), player1.getId(), 10, 500));
-            System.out.printf("PlayerDAO.buyGoods:\n    => %d total update players\n", updatedCount);
-
-            // So player 1 has to reduce the incoming quantity to two.
-            System.out.println("\nPlayerDAO.buyGoods:\n    => this trade will success");
-            updatedCount = (Integer)playerDAO.runTransaction(session,
-                    playerDAO.buyGoods(player2.getId(), player1.getId(), 2, 100));
-            System.out.printf("PlayerDAO.buyGoods:\n    => %d total update players\n", updatedCount);
-        } finally {
-            sessionFactory.close();
         }
     }
 }
---
+++ plain-java-hibernate/src/main/java/com/pingcap/PlayerBean.java
@@ -0,0 +1,76 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.pingcap;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * PlayerBean
+ *
+ * @author Icemap
+ * @date 2023/8/3
+ */
+@Entity
+@Table(name = "player_hibernate")
+class PlayerBean {
+    @Id
+    private String id;
+    @Column(name = "coins")
+    private Integer coins;
+    @Column(name = "goods")
+    private Integer goods;
+
+    public PlayerBean() {
+    }
+
+    public PlayerBean(String id, Integer coins, Integer goods) {
+        this.id = id;
+        this.coins = coins;
+        this.goods = goods;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getCoins() {
+        return coins;
+    }
+
+    public void setCoins(Integer coins) {
+        this.coins = coins;
+    }
+
+    public Integer getGoods() {
+        return goods;
+    }
+
+    public void setGoods(Integer goods) {
+        this.goods = goods;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("    %-8s => %10s\n    %-8s => %10s\n    %-8s => %10s\n",
+                "id", this.id, "coins", this.coins, "goods", this.goods);
+    }
+}
